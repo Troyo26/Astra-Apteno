8ff74500ebe4448424f8e95d88f1a336afa1ccae
Troyo26 <troyo1417@proton.me>
2026-07-17 07:03:58 +0300

    Rust Iced Async

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,72 @@
 #[derive(Debug, Clone)]
 enum Message {
     Refresh,
+    RefreshSucceeded,
+    RefreshFailed,
 }
+
+use std::time::Duration;
+
+async fn fake_refresh() {
+    std::thread::sleep(std::time::Duration::from_secs(2));
+}
+
 struct AppState {
     connection_state: ConnectionState,
 }
+
 enum ConnectionState {
     Refreshing,
     Connected,
     Disconnected,
 }
-fn update(: &mut bool, message: Message) {
+
+use iced::widget::{button, column, container, row, text};
+use iced::{Element, Fill, Task};
+
+fn update(state: &mut AppState, message: Message) -> Task<Message> {
     match message {
-        Message::Connect => *connected = true,
+        Message::Refresh => {
+            state.connection_state = ConnectionState::Refreshing;
+
+            Task::perform(fake_refresh(), |_| Message::RefreshSucceeded)
+        }
+
+        Message::RefreshSucceeded => {
+            state.connection_state = ConnectionState::Connected;
+
+            Task::none()
+        }
+
+        Message::RefreshFailed => {
+            state.connection_state = ConnectionState::Disconnected;
+
+            Task::none()
+        }
     }
 }
 
-use iced::widget::{button, column, container, row, text};
-use iced::{Element, Fill};
+fn status_text(state: &ConnectionState) -> &'static str {
+    match state {
+        ConnectionState::Refreshing => "Refreshing",
+        ConnectionState::Connected => "Connected",
+        ConnectionState::Disconnected => "Disconnected",
+    }
+}
 
-fn status_text(status: AppState) -> &'static str {}
-fn view(AppState { connected }: &mut bool) -> Element<'_, Message> {
+fn init() -> AppState {
+    AppState {
+        connection_state: ConnectionState::Disconnected,
+    }
+}
+
+fn view(state: &AppState) -> Element<'_, Message> {
     container(
         column![
             text("Astra Apteno"),
             text("World State"),
-            row![text("Status: "), text(status_text(*status)),].spacing(10),
-            button(text("Refresh")).on_press(Message::Refresh)),
+            row![text("Status: "), text(status_text(&state.connection_state)),].spacing(10),
+            button(text("Refresh")).on_press(Message::Refresh),
         ]
         .spacing(20),
     )
@@ -37,5 +77,5 @@ fn view(AppState { connected }: &mut bool) -> Element<'_, Message> {
 }
 
 pub fn main() -> iced::Result {
-    iced::run(update, view)
+    iced::application(init, update, view).run()
 }
